7cde9dd7c9b92da8abd27a86b9394b38c493923f
Jenaro <jenaroaaugusto@outlook.com>
2022-12-01 21:25:26 -0300

    Estudando parte 1

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+function Banner() {
+    // JSX - JavaScript XML
+    // Parece HTML, mas é JavaScript e como o react ler e transforma em HTML
+    return (
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="Banner principal" />
+        </header>
+    )
+}
+
+export default Banner
